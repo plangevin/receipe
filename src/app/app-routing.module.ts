4cb5752468ea2f4a532f61eef48e3326672d6357
Philippe Langevin <philippe.langevin@gmail.com>
2018-12-28 14:41:45 -0500

    Forgot this change  for the canLoad auth guard fix of previous commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
     { path: 'recipes',       loadChildren: './recipes/recipes.module#RecipesModule', canLoad:[AuthGuard] },
