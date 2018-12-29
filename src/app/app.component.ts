d6d8d1d779e593860b01eadf3cf0e4f0218ff27d
Philippe Langevin <philippe.langevin@gmail.com>
2018-12-29 13:34:53 -0500

    Removed useless/dead code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,11 @@ import * as firebase from 'firebase';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedFeature = 'recipe';
 
   ngOnInit() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCYnyP1TRrpE-5LsCbR3zpNRx1a-CBhZZU',
       authDomain: 'ng-recipe-book-ef92e.firebaseapp.com'
     });
-  }
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
   }
 }
